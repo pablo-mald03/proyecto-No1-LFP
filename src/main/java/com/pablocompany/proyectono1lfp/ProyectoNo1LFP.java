1bfd34042e6aaa0f07705ce9ab7f39a78ce5adcc
Pablo_Maldonado <pablomaldonadodev@gmail.com>
2025-09-25 14:45:34 -0600

    Se ha definido la interfaz principal de la aplicacion Primer commit del Proyecto

--- src/main/java/com/pablocompany/proyectono1lfp/ProyectoNo1LFP.java
+++ src/main/java/com/pablocompany/proyectono1lfp/ProyectoNo1LFP.java
@@ -3,7 +3,7 @@
  */
 package com.pablocompany.proyectono1lfp;
 
-import java.util.Scanner;
+import com.pablocompany.proyectono1lfp.frontend.aplicacion.MenuPrincipal;
 
 /**
  *
@@ -14,18 +14,8 @@ public class ProyectoNo1LFP {
     public static void main(String[] args) {
 
         //Prueba de comparacion de expresiones regulares
-        System.out.println("Escriba ");
-
-        Scanner prueba = new Scanner(System.in);
-
-        String pruebaPalabra = prueba.nextLine().trim();
-
-        if (pruebaPalabra.matches("^a(a|b|c|d|e)*$")) {
-
-            System.out.println("Se coincide");
-        } else {
-            System.out.println("no coincide");
-        }
+        MenuPrincipal menu = new MenuPrincipal();
+        menu.setVisible(true);
 
     }
 
---
+++ src/main/java/com/pablocompany/proyectono1lfp/backend/excepciones/AnalizadorLexicoException.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.pablocompany.proyectono1lfp.backend.excepciones;
+
+/**
+ *
+ * @author pablo
+ */
+//Excepcion propia del analizador lexico
+public class AnalizadorLexicoException extends Exception {
+
+    public AnalizadorLexicoException() {
+    }
+
+    public AnalizadorLexicoException(String message) {
+        super(message);
+    }
+
+}
